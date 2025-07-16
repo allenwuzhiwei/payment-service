4cf3d3d86e22ca33c061e0532a324c7a1c53d1a7
SONG YINRUI <78004370+RickSong218@users.noreply.github.com>
2025-07-16 18:19:57 +0800

    Update ApiResponse file

--- src/main/java/com/nusiss/paymentservice/config/ApiResponse.java
+++ src/main/java/com/nusiss/paymentservice/config/ApiResponse.java
@@ -17,6 +17,9 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> success() {
         return new ApiResponse<>(true, "success", null);
     }
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, message, data);
+    }
     public static <T> ApiResponse<T> error(String message) {
         return new ApiResponse<>(false, message, null);
     }
