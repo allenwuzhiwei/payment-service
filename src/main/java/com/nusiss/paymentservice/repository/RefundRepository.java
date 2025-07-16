047498f5d55cde744984986fa44194a5ad453802
SONG YINRUI <78004370+RickSong218@users.noreply.github.com>
2025-07-17 00:17:06 +0800

    Update RefundRepository with custom query to find refund by paymentId

--- src/main/java/com/nusiss/paymentservice/repository/RefundRepository.java
+++ src/main/java/com/nusiss/paymentservice/repository/RefundRepository.java
@@ -2,7 +2,15 @@ package com.nusiss.paymentservice.repository;
 
 import com.nusiss.paymentservice.entity.Refund;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface RefundRepository extends JpaRepository<Refund, Long> {
-    // 目前可使用默认方法
+
+    /*
+     根据 paymentId 查询退款记录
+     */
+    Optional<Refund> findByPaymentId(Long paymentId);
 }
